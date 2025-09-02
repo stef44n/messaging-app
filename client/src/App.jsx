a1fda942431f7eee8d79ddd9b5d4a1b9d8c21f45
Stefan <stefanseifeddine@gmail.com>
2025-09-02 22:36:08 +0100

    Add App

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+import { Link } from "react-router-dom";
+import { logout } from "./services/auth";
+
+export default function App() {
+    return (
+        <div className="p-6">
+            <h1 className="text-3xl font-bold mb-6">Messaging App</h1>
+            <nav className="flex gap-4">
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Signup</Link>
+                <Link to="/profile">Profile</Link>
+                <Link to="/inbox">Inbox</Link>
+                <button
+                    onClick={() => {
+                        logout();
+                        window.location.href = "/login";
+                    }}
+                >
+                    Logout
+                </button>
+            </nav>
+        </div>
+    );
+}
